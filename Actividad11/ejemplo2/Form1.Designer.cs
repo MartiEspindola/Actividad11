1b6cb4eec193a75301ddb72e5e0532037090d165
MartiEspindola <martinaespindola18@gmail.com>
2025-06-06 14:20:42 +0200

    ejemplo2

--- Actividad11/ejemplo2/Form1.Designer.cs
+++ Actividad11/ejemplo2/Form1.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            btnRegistrar_Click = new Button();
+            btnPromedio = new Button();
+            tbValor = new TextBox();
+            lbValor = new Label();
+            lbPromedio = new Label();
+            tbPromedio = new TextBox();
+            SuspendLayout();
+            // 
+            // btnRegistrar_Click
+            // 
+            btnRegistrar_Click.Location = new Point(344, 75);
+            btnRegistrar_Click.Name = "btnRegistrar_Click";
+            btnRegistrar_Click.Size = new Size(75, 23);
+            btnRegistrar_Click.TabIndex = 0;
+            btnRegistrar_Click.Text = "Registrar";
+            btnRegistrar_Click.UseVisualStyleBackColor = true;
+            btnRegistrar_Click.Click += button1_Click;
+            // 
+            // btnPromedio
+            // 
+            btnPromedio.Location = new Point(344, 122);
+            btnPromedio.Name = "btnPromedio";
+            btnPromedio.Size = new Size(75, 38);
+            btnPromedio.TabIndex = 1;
+            btnPromedio.Text = "Calcular Promedio";
+            btnPromedio.UseVisualStyleBackColor = true;
+            btnPromedio.Click += btnPromedio_Click;
+            // 
+            // tbValor
+            // 
+            tbValor.Location = new Point(161, 75);
+            tbValor.Name = "tbValor";
+            tbValor.Size = new Size(177, 23);
+            tbValor.TabIndex = 3;
+            // 
+            // lbValor
+            // 
+            lbValor.AutoSize = true;
+            lbValor.Location = new Point(117, 78);
+            lbValor.Name = "lbValor";
+            lbValor.Size = new Size(33, 15);
+            lbValor.TabIndex = 4;
+            lbValor.Text = "Valor";
+            // 
+            // lbPromedio
+            // 
+            lbPromedio.AutoSize = true;
+            lbPromedio.Location = new Point(117, 145);
+            lbPromedio.Name = "lbPromedio";
+            lbPromedio.Size = new Size(59, 15);
+            lbPromedio.TabIndex = 5;
+            lbPromedio.Text = "Promedio";
+            // 
+            // tbPromedio
+            // 
+            tbPromedio.Location = new Point(117, 175);
+            tbPromedio.Multiline = true;
+            tbPromedio.Name = "tbPromedio";
+            tbPromedio.Size = new Size(257, 85);
+            tbPromedio.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(555, 303);
+            Controls.Add(tbPromedio);
+            Controls.Add(lbPromedio);
+            Controls.Add(lbValor);
+            Controls.Add(tbValor);
+            Controls.Add(btnPromedio);
+            Controls.Add(btnRegistrar_Click);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnRegistrar_Click;
+        private Button btnPromedio;
+        private Button button3;
+        private TextBox tbValor;
+        private Label lbValor;
+        private Label lbPromedio;
+        private TextBox tbPromedio;
     }
 }
--- Actividad11/ejemplo2/Form1.cs
+++ Actividad11/ejemplo2/Form1.cs
@@ -2,9 +2,35 @@ namespace ejemplo2
 {
     public partial class Form1 : Form
     {
+        double acumulador = 0;
+        int contador = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+
+            acumulador += Convert.ToDouble(tbValor.Text);
+            contador++;
+
+            tbValor.Clear();
+        }
+
+        private void btnPromedio_Click(object sender, EventArgs e)
+        {
+            double promedio = acumulador / contador;
+            lbPromedio.Text = $"{promedio:f2}";
+
+            tbPromedio.Text = $@"Promedio:
+{promedio,10:f2}";
         }
     }
 }
